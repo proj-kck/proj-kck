271e41cdb90767ee27d48cece221af127c03fa04
kbarker <kbarker.webdev@gmail.com>
2022-08-09 14:37:44 -0500

    feat(db/init_db.js)Create Tables, Create Product, Seed Inital products

--- db/init_db.js
+++ db/init_db.js
@@ -1,5 +1,7 @@
 const {
   client,
+  users,
+  products
   // declare your model imports here
   // for example, User
 } = require('./');
@@ -8,9 +10,32 @@ async function buildTables() {
   try {
     client.connect();
 
-    // drop tables in correct order
+    console.log('Building tables...');
 
-    // build tables in correct order
+    await client.query(`
+      DROP TABLE IF EXISTS users;
+      DROP TABLE IF EXISTS products;
+    `)
+
+    await client.query(`
+      CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL
+      );
+    `);
+
+    await client.query(`
+      CREATE TABLE products (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) UNIQUE NOT NULL,
+        description TEXT NOT NULL,
+        price VARCHAR(255) NOT NULL,
+        category VARCHAR(255) NOT NULL,
+        img TEXT
+      );
+    `);
   } catch (error) {
     throw error;
   }
@@ -18,7 +43,16 @@ async function buildTables() {
 
 async function populateInitialData() {
   try {
-    // create useful starting data by leveraging your
+
+    console.log('Creating products...')
+    const result = await products.createProduct({
+      name: 'Yuengling',
+      description: '12pk Bottles - Light lager beer', 
+      price: '12.99', 
+      category: 'beer', 
+      img:'https://images.heb.com/is/image/HEBGrocery/005666012?fit=constrain,1&wid=800&hei=800&fmt=jpg&qlt=85,0&resMode=sharp2&op_usm=1.75,0.3,2,0'})
+    console.log(result)
+      // create useful starting data by leveraging your
     // Model.method() adapters to seed your db, for example:
     // const user1 = await User.createUser({ ...user info goes here... })
   } catch (error) {
