5568853b471fb67d8718528ed9e566d97edb9c24
kbarker <kbarker.webdev@gmail.com>
2022-08-10 08:35:23 -0500

    added isAdmin column to users tables

--- db/init_db.js
+++ db/init_db.js
@@ -20,7 +20,8 @@ async function buildTables() {
         id SERIAL PRIMARY KEY,
         username VARCHAR(255) UNIQUE NOT NULL,
         password VARCHAR(255) NOT NULL,
-        email VARCHAR(255) UNIQUE NOT NULL
+        email VARCHAR(255) UNIQUE NOT NULL,
+        "isAdmin" BOOLEAN DEFAULT false
       );
     `);
 
--- db/models/users.js
+++ db/models/users.js
@@ -8,16 +8,16 @@ module.exports = {
   createUser
 };
 
-async function createUser({username, password, email}) {
+async function createUser({username, password, email, isAdmin = false}) {
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const { rows: [user] } = await client.query(`
-      INSERT INTO users (username, password, email)
-      VALUES($1, $2, $3)
+      INSERT INTO users (username, password, email, "isAdmin")
+      VALUES($1, $2, $3, $4)
       ON CONFLICT DO NOTHING
       RETURNING *;
-    `, [username, hashedPassword, email]);
+    `, [username, hashedPassword, email, isAdmin]);
 
     return user;
   } catch (error) {
