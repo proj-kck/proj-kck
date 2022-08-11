2e2a590b3d915b711187b1da31cef595138cd948
Ketsy <jequetsey3@gmail.com>
2022-08-11 15:22:03 -0400

    users functions

--- db/models/users.js
+++ db/models/users.js
@@ -2,23 +2,19 @@
 const client = require('../client');
 const bcrypt = require('bcrypt');
 
-module.exports = {
-  // add your database adapter fns here
-  getAllUsers,
-  createUser
-};
+
 
 async function createUser({username, password, email, isAdmin = false}) {
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
-
+    
     const { rows: [user] } = await client.query(`
-      INSERT INTO users (username, password, email, "isAdmin")
-      VALUES($1, $2, $3, $4)
-      ON CONFLICT DO NOTHING
-      RETURNING *;
+    INSERT INTO users (username, password, email, "isAdmin")
+    VALUES($1, $2, $3, $4)
+    ON CONFLICT DO NOTHING
+    RETURNING *;
     `, [username, hashedPassword, email, isAdmin]);
-
+    
     return user;
   } catch (error) {
     throw error;
@@ -27,4 +23,61 @@ async function createUser({username, password, email, isAdmin = false}) {
 
 async function getAllUsers() {
   /* this adapter should fetch a list of users from your db */
+  try {
+    const { rows } = await client.query(`
+    SELECT id, username, email, "isAdmin"
+    FROM users;
+    `);
+    return rows;
+  } catch (error) {
+    throw error;
+  }
 }
+async function getUserById(userId) {
+  try {
+    const {rows: [user] } = await client.query(`
+    SELECT id, username, email, "isAdmin"
+    FROM users
+    WHERE id=${userId}
+    `);
+    if (!user) {
+      throw {
+        name: "UserNotFoundError",
+        message: "A user with that id does not exist"
+      }
+    }
+    user.products = await getProductsByUser(userId);
+    
+    return user;
+  } catch (error) {
+    throw error;
+  }
+} 
+async function updateUser(id, fields = {}) {
+  const setString = Object.keys(fields).map(
+    (key, index) => `"${ key }"=$${ index + 1 }`
+    ).join(', ');
+    if (setString.length === 0) {
+      return;
+    }
+    try {
+      const { rows: [ user ]} = await client.query(`
+      UPDATE users
+      SET ${ setString }
+      WHERE id=${ id }
+      RETURNING *;
+      `, Object.values(fields));
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  }
+  
+module.exports = {
+    // add your database adapter fns here
+  client,
+  getAllUsers,  
+  createUser,
+  getUserById,
+  updateUser,
+};
