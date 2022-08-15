0e306b1e137ead0c8e31b2da132e712b764fc9ae
cgudge <102206770+cgudge@users.noreply.github.com>
2022-08-15 07:09:14 -0800

    readded getProductById it was lost in a merge conflict

--- src/axios-services/index.js
+++ src/axios-services/index.js
@@ -34,3 +34,32 @@ export async function getAllProducts() {
 		return data;
 	} catch (error) {}
 }
+export async function getProductById(id) {
+	try {
+		const { data } = await axios.get(`/api/products/${id}`);
+		return data;
+	} catch (error) {}
+}
+
+export async function login(username, password) {
+	try {
+		const { data } = await axios.post('/api/login', { username, password });
+		return data;
+	} catch (error) {
+		throw error;
+	}
+}
+
+export async function register(username, password, email, is_admin) {
+	try {
+		const { data } = await axios.post('/api/register', {
+			username,
+			password,
+			email,
+			is_admin,
+		});
+		return data;
+	} catch (error) {
+		throw error;
+	}
+}
