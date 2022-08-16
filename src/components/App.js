7dde0f110b57f97919412e2ea625b37136b3ed33
kbarker <kbarker.webdev@gmail.com>
2022-08-16 09:41:06 -0500

    fixed bug

--- src/components/App.js
+++ src/components/App.js
@@ -64,7 +64,7 @@ const App = () => {
 									setPassword={setPassword}
 									username={username}
 									password={password}
-									users={users}
+									// users={users}
 									loggedInUser={loggedInUser}
 									setLoggedInUser={setLoggedInUser}
 								/>
@@ -83,7 +83,7 @@ const App = () => {
 									setPassword={setPassword}
 									username={username}
 									password={password}
-									users={users}
+									// users={users}
 									loggedInUser={loggedInUser}
 									setLoggedInUser={setLoggedInUser}
 								/>
