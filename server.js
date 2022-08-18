ce632e57290c4ad188cf52b9dc15370d6b2bfe75
Ketsy <jequetsey3@gmail.com>
2022-08-18 10:34:41 -0400

    check

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const session = require('express-session');
-const app = express();
-// const { cartRouter } = require('./src/')
+// const express = require('express');
+// const session = require('express-session');
+// const app = express();
+// // const { cartRouter } = require('./src/')
 
-app.use(express.json());
+// app.use(express.json());
 
-app.use(
-    session({
-        secret: "secrets",
-        resave: false,
-        saveUninitialized: false
-    })
-);
-app.use((req, res, next) => {
-    console.log(`${req.method}:${req.url}`);
-    next();
-});
-// app.use('/api/carts', cartsRouter)
-app.get("/", (req, res) => {
-    console.log(req.session);
-    console.log('the actual session id: ', req.sessionID)
-})
+// app.use(
+//     session({
+//         secret: "secrets",
+//         resave: false,
+//         saveUninitialized: false
+//     })
+// );
+// app.use((req, res, next) => {
+//     console.log(`${req.method}:${req.url}`);
+//     next();
+// });
+// // app.use('/api/carts', cartsRouter)
+// app.get("/", (req, res) => {
+//     console.log(req.session);
+//     console.log('the actual session id: ', req.sessionID)
+// })
