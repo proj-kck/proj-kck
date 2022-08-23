9fa3957001affad07e67ceb5eacf5d5d015b964e
kbarker <kbarker.webdev@gmail.com>
2022-08-23 11:58:33 -0500

    added our links && added success messages

--- src/components/index.js
+++ src/components/index.js
@@ -2,9 +2,5 @@ import { Snackbar } from '@mui/material';
 
 export { default as App } from './App';
 
-export const successMsg = (element) => {
-        element.className = "show";
-        setTimeout(function(){ element.className = element.className.replace("show", ""); }, 3000);
-}
 
 
